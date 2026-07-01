a078e143264c7182a46ea35bda6941baccfce9c5
Kimhyunjin4455 <dp4455@naver.com>
2026-07-01 22:37:08 +0900

    fix: 주문 상태 변경 시 재고 차감에 따른 재고 연산 로직 제거

--- db/procedure/sp_order_create.sql
+++ db/procedure/sp_order_create.sql
@@ -1,5 +1,6 @@
--- 상품 주문 등록                                                             
--- 선행: sp_inventory_update                                                                                                                                                 
+-- 상품 주문 등록                                                                                
+-- 선행: 없음                                                                                    
+                                                                                                   
 CREATE OR REPLACE PROCEDURE sp_order_create(                                                     
     p_customer_id BIGINT                                                                         
     , p_product_id  BIGINT                                                                         
@@ -8,32 +9,20 @@ CREATE OR REPLACE PROCEDURE sp_order_create(
 )                                                                                                
 LANGUAGE plpgsql                                                                                 
 AS $$                                                                                            
-BEGIN                                                                                            
-    -- 재고 조회 (행 잠금)                                                                       
-    PERFORM 1                                                                                    
-    FROM t_inventory                                                                             
-    WHERE product_id = p_product_id                                                              
-    FOR UPDATE;                                                                                  
-                                                                                                
-    -- 재고 차감                                                                                 
-    CALL sp_inventory_update(                                                                    
-        p_product_id      => p_product_id                                                        
-    , p_quantity_change => -p_quantity                                                         
-    );                                                                                           
-                                                                                                
-    -- 주문 등록                                                                                 
-    INSERT INTO t_product_order (                                                                
-        customer_id                                                                              
-    , product_id                                                                               
-    , quantity                                                                                 
-    , order_status                                                                             
-    , ordered_at                                                                               
-    ) VALUES (                                                                                   
-        p_customer_id                                                                            
-    , p_product_id                                                                             
-    , p_quantity                                                                               
-    , 'ORDERED'                                                                                
-    , p_ordered_at                                                                             
-    );                                                                                           
-END;                                                                                             
-$$;                              
+  BEGIN                                                                                            
+      -- 주문 등록                                                                                 
+      INSERT INTO t_product_order (                                                                
+          customer_id                                                                              
+        , product_id                                                                               
+        , quantity                                                                                 
+        , order_status                                                                             
+        , ordered_at                                                                               
+      ) VALUES (                                                                                   
+          p_customer_id                                                                            
+        , p_product_id                                                                             
+        , p_quantity                                                                               
+        , 'ORDERED'                                                                                
+        , p_ordered_at                                                                             
+      );                                                                                           
+  END;                                                                                             
+  $$;           
